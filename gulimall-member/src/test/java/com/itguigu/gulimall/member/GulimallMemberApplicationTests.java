69d04ad26bab7b777e7200a38abc77fc178cb2fe
zzw <1032983946@qq.com>
2020-11-15 21:02:08 +0800

    the first commit

---
+++ gulimall-member/src/test/java/com/itguigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itguigu.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
